86b3523887e82b0ffadf1c7a77935f7484f889a8
serge <stk77@yandex.ru>
2021-11-16 18:40:38 +0300

    lesson-01

--- core/src/ru/stk/RoadRace.java
+++ core/src/ru/stk/RoadRace.java
@@ -1,6 +1,7 @@
 package ru.stk;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -12,17 +13,19 @@ public class RoadRace extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("pixel5.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 0.45f, 0, 1);
+	//	Gdx.graphics.getHeight() размер экрана
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
+
 	@Override
 	public void dispose () {
 		batch.dispose();
