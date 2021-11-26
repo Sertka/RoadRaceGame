d34ef17a073a95aa7f12d1301bc1bae8be0ef926
serge <stk77@yandex.ru>
2021-11-26 15:24:58 +0300

    lesson-04

--- core/src/ru/stk/screen/impl/GameScreen.java
+++ core/src/ru/stk/screen/impl/GameScreen.java
@@ -1,22 +1,36 @@
 package ru.stk.screen.impl;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.stk.math.Rect;
 import ru.stk.screen.BaseScreen;
 import ru.stk.sprite.impl.Background;
+import ru.stk.sprite.impl.Ship;
+import ru.stk.sprite.impl.Star;
 
 public class GameScreen extends BaseScreen {
 
     private Texture bg;
     private Background background;
+    private TextureAtlas atlas;
+    private static final int STAR_COUNT = 64;
+    private Star[] stars;
+    private Ship ship;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
         background = new Background(bg);
+
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+        ship = new Ship(atlas);
     }
 
     @Override
@@ -30,12 +44,17 @@ public class GameScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        ship.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         super.dispose();
         bg.dispose();
+        atlas.dispose();
     }
 
     @Override
@@ -49,12 +68,33 @@ public class GameScreen extends BaseScreen {
     }
 
     private void update(float delta) {
-
+        for (Star star : stars) {
+            star.update(delta);
+        }
+        ship.update(delta);
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+//            batch.setColor(Color.YELLOW);
+            star.draw(batch);
+//            batch.setColor(Color.CLEAR);
+        }
+        ship.draw(batch);
         batch.end();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        ship.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        ship.keyUp(keycode);
+        return false;
     }
 }
